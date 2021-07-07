e171f1ab14a5c2b2fc18a39ea0828ba6bf21cd53
fotomassimo <maksym.pavliuchenko@gmail.com>
2021-07-07 10:18:24 +0300

    commit #6: logger fixed

--- src/main/java/football/tickets/app/config/AppConfig.java
+++ src/main/java/football/tickets/app/config/AppConfig.java
@@ -1,11 +1,8 @@
 package football.tickets.app.config;
 
-import football.tickets.app.security.CustomUserDetailsService;
 import java.util.Properties;
 import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -55,10 +52,5 @@ public class AppConfig {
     @Bean
     public PasswordEncoder getEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public Logger getLogger() {
-        return LogManager.getLogger(CustomUserDetailsService.class);
     }
 }
